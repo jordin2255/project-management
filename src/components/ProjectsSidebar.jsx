23019542366da154f3a02a72feadda10eba7d06b
jordin <jordinmathew.mec@gmail.com>
2024-10-18 06:30:48 +1000

    no project to adding new project transition created

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,9 +1,9 @@
 import Button from "./Button";
-export default function ProjectsSidebar(){
+export default function ProjectsSidebar({onStartAddProject}){
   return <aside className="w-1/3px px-8 py-16 bg-emerald-300 rounded-r-xl md:w-72">
     <h2 className="mb-8 font-bold uppercase md:text-xl">Your Projects</h2>
     <div>
-      <Button>+ Add Project</Button>
+      <Button onClick={onStartAddProject}>+ Add Project</Button>
     </div>
     <ul></ul>
   </aside>
