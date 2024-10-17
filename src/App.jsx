420a956b03ade1b0fb2d80be054cdd3c3ff9cc90
jordin <jordinmathew.mec@gmail.com>
2024-10-18 09:43:47 +1000

    collecting user inputs

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,19 @@ function App() {
       return {...prevState,selectedProjectId: null};
     });
   }
+
+  function handleAddProject(projectData){
+    setProjectsState(prevState=>{
+      const newProject={...projectData,id:Math.random()};
+      return{
+      ...prevState,
+      projects:[...prevState.projects,newProject]}});
+  }
   let content;
   if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   } else if (projectsState.selectedProjectId === null) {
-    content = <NewProject />;
+    content = <NewProject onAdd={handleAddProject}/>;
   }
   return (
     <main className="h-screen my-8 flex gap-8">
